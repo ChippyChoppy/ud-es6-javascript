2620a115145f538921b30e0f10169de143b5fc30
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-09-17 15:24:51 -0400

    sec 6 done

--- forEachHelper/forEachSec6.js
+++ forEachHelper/forEachSec6.js
@@ -5,3 +5,17 @@
  * print the sum variable
  */
 
+const nums = [1,2,3,4,5];
+
+let sum = 0;
+
+nums.forEach(function(num) {
+    sum += num;
+});
+
+function adder(number) {
+    sum += number
+}
+nums.forEach(adder) // <-- reference to the function, not using parens b/c that would immediately call adder and pass the result to forEach instead of forEach calling it.
+
+console.log(sum);
