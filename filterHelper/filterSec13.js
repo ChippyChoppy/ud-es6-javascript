0a5806f0ad7eb8fdd092b9c348c53ac15e3f622b
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-09-17 16:31:42 -0400

    section 13

---
+++ filterHelper/filterSec13.js
@@ -0,0 +1,24 @@
+/** 
+ * Choosing when to use Filter
+ * 
+ * given a particular post, how to return all comments associated with that post?
+ */
+
+const post = { id: 4, title: 'cool title' };
+const comments = [
+    {postId: 4, content: 'awesome post' },
+    {postId: 3, content: 'it was aight'},
+    {postId: 4, content: 'sweeet!'}
+];
+
+function commentsForPost(post, comments) {
+    return comments.filter(function(comment) {
+        return comment.postId === post.id;
+    })
+}
+
+console.log(commentsForPost(post, comments));
+
+/**
+ * 
+ */
