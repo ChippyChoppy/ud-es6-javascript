cf5abbf82c6f372d289f6d480b231c6337ccd975
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-09-17 15:55:33 -0400

    exercise 3

---
+++ mapHelper/exercise3.js
@@ -0,0 +1,16 @@
+/**
+ * Using map, create a new array that contains the 'height' property of each object. 
+ * Assign this new array to the variable 'heights'.
+ * Don't forget to use the 'return' keyword in the function!
+ */
+/* ---*** Provided Array ***--- */
+const images = [
+    { height: '34px', width: '39px' },
+    { height: '54px', width: '19px' },
+    { height: '83px', width: '75px' },
+];
+
+/*** My code ***/
+const heights = images.map(function(image) {
+    return image.height
+})
